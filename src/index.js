f6b524bac2bed9d166cae1cb437c3db1d893b703
ann-sm <annsmirnovva@gmail.com>
2024-10-01 22:44:23 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -39,7 +39,37 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+    let newArray = expr.match(/.{1,10}/g);
+  
+    let phrase = '';
+    let morseChar = '';
+    
+    for (let i = 0; i < newArray.length; i++) {
+      const encodedBinary = newArray[i];
+      
+      if (encodedBinary !== '**********') {
+        
+         for (let j = 0; j < encodedBinary.length ; j = j + 2) {
+           if (encodedBinary.slice(j, j + 2) === '00') {
+         
+          } else if (encodedBinary.slice(j, j + 2) === '10') {
+            morseChar = `${morseChar}.`;
+          } else if (encodedBinary.slice(j, j + 2) === '11') {
+            morseChar = `${morseChar}-`;
+          }
+        } 
+        const char = MORSE_TABLE[morseChar];
+        phrase = `${phrase}${char}`;
+        morseChar = '';
+      } else {
+        morseChar = `${morseChar} `;
+        const char = morseChar;
+        phrase = `${phrase}${char}`;
+        morseChar = '';
+      }
+    }
+    return phrase;
+} 
 
 module.exports = {
     decode
